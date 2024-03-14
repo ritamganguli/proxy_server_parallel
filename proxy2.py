453d8fbdd7b05124c8815eef9d5f111f112a5c90
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-03-14 21:09:47 +0530

    Update proxy2.py

--- proxy2.py
+++ proxy2.py
@@ -32,6 +32,7 @@ lt_options["smartUI.project"] = "test"
 lt_options["name"] = "basic_unit_selinium"
 lt_options["w3c"] = True
 lt_options["tunnel"]=True
+lt_options["tunnelName"]="Ritam13456"
 lt_options["plugin"] = "python-python"
 options.set_capability("LT:Options", lt_options)
 
